5d74403c8ef4a91d191f42e3d08e983ceb59543a
Ilia <ilia@bookjane.com>
2019-09-11 22:05:33 -0400

    basic project setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+  const [rooms, setRooms] = useState([]);
+
+  return (
+    <div>
+      Home
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/routes.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import styled from 'styled-components';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Home from './Pages/Home';
+import FAQ from './Pages/FAQ';
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Routes = () => (
+  <Router>
+    <Wrapper>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={Home}
+        />
+
+        <Route
+          exact
+          path="/faq"
+          component={FAQ}
+        />
+      </Switch>
+      <ToastContainer
+        autoClose={3500}
+      />
+    </Wrapper>
+  </Router>
+);
+
+export default Routes;
